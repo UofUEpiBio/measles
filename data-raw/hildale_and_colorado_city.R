c6dd577d899868b18f15aa96d0a8e0676b6ecb43
George G. Vega Yon <g.vegayon@gmail.com>
2025-12-09 19:31:04 +0000

    Renaming to reflect better

--- data-raw/hildale_and_colorado_city.R
+++ data-raw/hildale_and_colorado_city.R
@@ -1,4 +1,4 @@
-# This file generates the data for the southwest Utah measles vignette
+# This file generates the data for the Hildale and Colorado City measles vignette
 # The original code was adapted from the multigroup.vaccine package,
 # in a vignette by @JakeWags:
 # https://github.com/EpiForeSITE/multigroup-vaccine/blob/54acee568fbb0666231ab59f19d33841337d7402/vignettes/hildale_and_colorado_city.Rmd
@@ -50,88 +50,88 @@ colorado_city_data <- city_data_list[["Colorado City town, Arizona"]][
 ]
 
 # Preparing both in a single data.frame
-southwest_utah <- rbind(
+hildale_and_colorado_city <- rbind(
   with(hildale_data, data.frame(city = "Hildale, UT", age_labels, age_pops)),
   with(colorado_city_data, data.frame(city = "Colorado City, AZ", age_labels, age_pops))
 ) |> data.table()
 
-southwest_utah <- southwest_utah[,
+hildale_and_colorado_city <- hildale_and_colorado_city[,
   .(agepops = sum(age_pops)), by = "age_labels"
 ]
 
 # Preparing the data for Damon's package
-southwest_utah[, agelims := as.integer(gsub("to.+", "", age_labels))]
-southwest_utah[is.na(agelims), agelims := 70L]
+hildale_and_colorado_city[, agelims := as.integer(gsub("to.+", "", age_labels))]
+hildale_and_colorado_city[is.na(agelims), agelims := 70L]
 
 schoolagegroups <- c(2, 2, 3, 3, 4, 4)
 schoolpops <- c(100, 100, 200, 200, 200, 200)
 
-southwest_utah_matrix <- contactMatrixAgeSchool(
-  southwest_utah$agelims,
-  southwest_utah$agepops,
+hildale_and_colorado_city_matrix <- contactMatrixAgeSchool(
+  hildale_and_colorado_city$agelims,
+  hildale_and_colorado_city$agepops,
   schoolagegroups,
   schoolpops, schportion = 0.7
 )
 
-southwest_utah_matrix |> round(2)
+hildale_and_colorado_city_matrix |> round(2)
 
 # Ensuring it is row-stochastic
 # Future version of epiworld may support non-row-stochastic matrices.
-southwest_utah_matrix <- southwest_utah_matrix / rowSums(southwest_utah_matrix)
+hildale_and_colorado_city_matrix <- hildale_and_colorado_city_matrix / rowSums(hildale_and_colorado_city_matrix)
 
 usethis::use_data(
-  southwest_utah_matrix,
+  hildale_and_colorado_city_matrix,
   internal = FALSE,
   overwrite = TRUE
 )
 
-# We now append the school populations to the southwest_utah data
+# We now append the school populations to the hildale_and_colorado_city data
 # substracting the school populations from the age groups
-schools <- southwest_utah[schoolagegroups]
+schools <- hildale_and_colorado_city[schoolagegroups]
 
 schools[, agepops := schoolpops]
 
 schools_pop <- schools[, .(school_p = sum(agepops)), by = .(age_labels)]
-southwest_utah <- merge(
-  southwest_utah,
+hildale_and_colorado_city <- merge(
+  hildale_and_colorado_city,
   schools_pop,
   all = TRUE,
   by = "age_labels"
 )
 
-southwest_utah[!is.na(school_p), agepops := agepops - school_p]
-southwest_utah[, school_p := NULL]
+hildale_and_colorado_city[!is.na(school_p), agepops := agepops - school_p]
+hildale_and_colorado_city[, school_p := NULL]
 
 # Relabeling data
 schools[, age_labels := paste0(age_labels, "s", .I)]
 
 # Putting all together
-southwest_utah <- rbind(
-  southwest_utah,
+hildale_and_colorado_city <- rbind(
+  hildale_and_colorado_city,
   schools
 )
-southwest_utah[age_labels == "70plus", age_labels := "70+"]
-southwest_utah[age_labels == "0to4", age_labels := "under5"]
+hildale_and_colorado_city[age_labels == "70plus", age_labels := "70+"]
+hildale_and_colorado_city[age_labels == "0to4", age_labels := "under5"]
 
 # Sorting according to the mixing matrix
 ids <- match(
-  colnames(southwest_utah_matrix), southwest_utah$age_labels
+  colnames(hildale_and_colorado_city_matrix), hildale_and_colorado_city$age_labels
 )
 
 ids <- ids[!is.na(ids)]
-southwest_utah <- southwest_utah[ids]
+hildale_and_colorado_city <- hildale_and_colorado_city[ids]
 
 # Ensuring ti works
 stopifnot(
-  all(southwest_utah$age_labels == colnames(southwest_utah_matrix))
+  all(hildale_and_colorado_city$age_labels == colnames(hildale_and_colorado_city_matrix))
 )
 
 # Adding a fake vaccination rate
-southwest_utah[, vacc_rate := 0.9]
-southwest_utah[grepl("s", age_labels), vacc_rate := 0.5]
+hildale_and_colorado_city[, vacc_rate := 0.9]
+hildale_and_colorado_city[grepl("s", age_labels), vacc_rate := 0.5]
 
 usethis::use_data(
-  southwest_utah,
+  hildale_and_colorado_city,
   internal = FALSE,
   overwrite = TRUE
 )
