3c93ed2703e31e82cf7484322d63541dc8a3071a
George G. Vega Yon <g.vegayon@gmail.com>
2026-01-26 12:07:38 -0700

    Updating data using multigroup.vaccine (#24)
    * Updating data using multigroup.vaccine
    
    * The package doesn't actually depend on multigroup.vaccine
    
    * Removing remotes (not needed)
    
    * Removing the epiworldR@main version

--- data-raw/short_creek.R
+++ data-raw/short_creek.R
@@ -1,108 +1,107 @@
-# This file generates the data for the Short Creek measles vignette
-# The original code was adapted from the multigroup.vaccine package,
-# in a vignette by @JakeWags:
-# https://github.com/EpiForeSITE/multigroup-vaccine/blob/54acee568fbb0666231ab59f19d33841337d7402/vignettes/hildale_and_colorado_city.Rmd
+# Most of this R script was created based on the vignette
+# by the multigroup.vaccine package.
+# Date: Monday, Jan 26, 2026
+# https://github.com/EpiForeSITE/multigroup-vaccine/blob/3047ebf568c9b2028336dc14af587a282de9e225/vignettes/experiments/shortcreek_age_school.Rmd
 
 library(multigroup.vaccine)
 library(socialmixr)
 library(data.table)
 
 # Load city data files
-hildale_path <- system.file(
-  "extdata", "hildale_ut_2023.csv", package = "multigroup.vaccine"
+hildale_path <- system.file("extdata", "hildale_ut_2023.csv", package = "multigroup.vaccine")
+colorado_city_path <- system.file("extdata", "colorado_city_az_2023.csv", package = "multigroup.vaccine")
+centennial_park_path <- system.file("extdata", "centennial_park_az_2023.csv", package = "multigroup.vaccine")
+
+
+# ## Measles Model Setup
+
+# For measles outbreak modeling, let's use the following age groups:
+
+agelims <- c(0, 1, 5, 12, 14, 18, 25, 45, 70)
+ageveff <- c(0.93, 0.93, rep(0.97, 6), 1)
+
+
+## Getting City Population Data
+
+
+hildale <- getCityData(
+  city_name = "Hildale city, Utah",
+  csv_path = hildale_path,
+  age_groups = agelims
 )
 
-colorado_city_path <- system.file(
-  "extdata", "colorado_city_az_2023.csv", package = "multigroup.vaccine"
+colorado_city <- getCityData(
+  city_name = "Colorado City town, Arizona",
+  csv_path = colorado_city_path,
+  age_groups = agelims
 )
 
-centennial_park_path <- system.file(
-  "extdata", "centennial_park_az_2023.csv", package = "multigroup.vaccine"
+centennial_park <- getCityData(
+  city_name = "Centennial Park CDP, Arizona",
+  csv_path = centennial_park_path,
+  age_groups = agelims
 )
 
-# Age groups based on ACS 5-year intervals, adjusted to avoid zero populations
-agelims <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70)
+agepops <- round(hildale$age_pops + colorado_city$age_pops + centennial_park$age_pops)
 
-cities <- c(
-  "Hildale city, Utah", "Colorado City town, Arizona",
-  "Centennial Park CDP, Arizona"
-)
 
-city_data_list <- list()
+## School data
 
-for (city in cities) {
-  if (grepl("Hildale", city)) {
-    csv_path <- hildale_path
-  } else if (grepl("Colorado City", city)) {
-    csv_path <- colorado_city_path
-  } else if (grepl("Centennial Park", city)) {
-    csv_path <- centennial_park_path
-  } else {
-    stop("Unknown city: ", city)
-  }
-
-  data <- getCityData(
-    city_name = city,
-    csv_path = csv_path,
-    age_groups = agelims
-  )
-
-  city_data_list[[city]] <- data
-
-}
-
-# We extract only the age and pops
-hildale_data <- city_data_list[["Hildale city, Utah"]][
-  c("age_labels", "age_pops")
-]
-
-colorado_city_data <- city_data_list[["Colorado City town, Arizona"]][
-  c("age_labels", "age_pops")
-]
-
-centenial_park_data <- city_data_list[["Centennial Park CDP, Arizona"]][
-  c("age_labels", "age_pops")
-]
-
-# Preparing both in a single data.frame
-short_creek <- rbind(
-  with(
-    hildale_data,
-    data.frame(city = "Hildale, UT", age_labels, age_pops)
-  ),
-  with(
-    colorado_city_data,
-    data.frame(city = "Colorado City, AZ", age_labels, age_pops)
-  ),
-  with(
-    centenial_park_data,
-    data.frame(city = "Centennial Park, AZ", age_labels, age_pops)
-  )
-) |> data.table()
-
-short_creek <- short_creek[,
-  .(agepops = sum(age_pops)), by = "age_labels"
-]
-
-# Preparing the data for Damon's package
-short_creek[, agelims := as.integer(gsub("to.+", "", age_labels))]
-short_creek[is.na(agelims), agelims := 70L]
 
 schoolpops <- c(250, 350, 190, 86, 150, 84, 114, 108, 205)
 schoolagegroups <- c(3, 3, 3, 4, 4, 4, 5, 5, 5)
 schoolvax <- c(16, 129, 80, 20, 55, 50, 27, 40, 93)
 
-short_creek_matrix <- contactMatrixAgeSchool(
-  short_creek$agelims,
-  short_creek$agepops,
-  schoolagegroups,
-  schoolpops, schportion = 0.7
-)
+knitr::kable(data.frame(school = c(paste0("elem", 1:3),
+  paste0("middle", 1:3),
+  paste0("high", 1:3)),
+enrolled = schoolpops,
+MMRcoverage = paste0(round(100 * schoolvax / schoolpops), "%")),
+row.names = FALSE, format = "markdown")
 
-short_creek_matrix |> round(2)
 
-# Ensuring it is row-stochastic
-# Future version of epiworld may support non-row-stochastic matrices.
+## Create contact matrix and immunization vector
+
+
+# Readjust the school populations to match the age data:
+for (a in unique(schoolagegroups)) {
+  inds <- which(schoolagegroups == a)
+  schoolpops[inds] <- round(agepops[a] * schoolpops[inds] / sum(schoolpops[inds]))
+}
+cm <- contactMatrixAgeSchool(agelims, agepops, schoolagegroups, schoolpops, schportion = 0.7)
+grouppops <- c(agepops[1:(min(schoolagegroups) - 1)],
+  schoolpops,
+  agepops[(max(schoolagegroups) + 1):length(agepops)])
+groupvax <- rep(0, nrow(cm))
+
+groupvax[1] <- 0 # Under 1 unvaccinated
+groupvax[2] <- grouppops[2] * sum(schoolvax[schoolagegroups == 3]) / sum(schoolpops[schoolagegroups == 3])
+groupvax[3:11] <- schoolvax
+groupvax[12] <- grouppops[12] * sum(schoolvax[schoolagegroups == 5]) / sum(schoolpops[schoolagegroups == 5])
+groupvax[13] <- grouppops[13] * mean(c(groupvax[12] / grouppops[12], 0.95))
+groupvax[14] <- grouppops[14] * 0.95
+groupvax[15] <- grouppops[15]
+
+groupveff <- rep(0.97, length(groupvax))
+groupveff[1:2] <- 0.93
+groupveff[length(groupveff)] <- 1
+groupimm <- round(groupvax * groupveff)
+
+knitr::kable(data.frame(group = rownames(cm),
+  size = grouppops,
+  immunity = paste0(round(100 * groupimm / grouppops), "%")),
+row.names = FALSE, format = "markdown")
+
+
+## Set up outbreak analysis
+## STOP()
+
+################################################
+# Short Creek Data Preparation
+################################################
+
+# Making it row-stochastic for use in epiworld
+short_creek_matrix <- cm
 short_creek_matrix <- short_creek_matrix / rowSums(short_creek_matrix)
 
 usethis::use_data(
@@ -111,53 +110,19 @@ usethis::use_data(
   overwrite = TRUE
 )
 
-# We now append the school populations to the short_creek data
-# substracting the school populations from the age groups
-schools <- short_creek[schoolagegroups]
-
-schools[, agepops := schoolpops]
-
-schools_pop <- schools[, .(school_p = sum(agepops)), by = .(age_labels)]
-short_creek <- merge(
-  short_creek,
-  schools_pop,
-  all = TRUE,
-  by = "age_labels"
+# Preparing the
+short_creek <- data.table(
+  age_labels = rownames(short_creek_matrix),
+  agepops = grouppops,
+  agelims = gsub(
+    ".+to([0-9]+).*", "\\1",
+    rownames(short_creek_matrix)
+  ) |> as.integer(),
+  vacc_rate = groupimm / grouppops
 )
 
-short_creek[!is.na(school_p), agepops := agepops - school_p]
-short_creek[, school_p := NULL]
-
-# Relabeling data
-schools[, age_labels := paste0(age_labels, "s", .I)]
-
-# Putting all together
-short_creek <- rbind(
-  short_creek,
-  schools
-)
-short_creek[age_labels == "70plus", age_labels := "70+"]
-short_creek[age_labels == "0to4", age_labels := "under5"]
-
-# Sorting according to the mixing matrix
-ids <- match(
-  colnames(short_creek_matrix), short_creek$age_labels
-)
-
-ids <- ids[!is.na(ids)]
-short_creek <- short_creek[ids]
-
-# Ensuring ti works
-stopifnot(
-  all(short_creek$age_labels == colnames(short_creek_matrix))
-)
-
-# Adding a fake vaccination rate
-short_creek[, vacc_rate := 0.93]
-short_creek[
-  grepl("s", age_labels),
-  vacc_rate := schoolvax / schoolpops
-]
+short_creek[age_labels == "under1", agelims := 1L]
+short_creek[age_labels == "70+", agelims := 90L]
 
 usethis::use_data(
   short_creek,
